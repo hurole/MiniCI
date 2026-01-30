05dcece2161192df34f52333b3f04cb62ecf46b7
hr.deng <hr.deng@alphabets.cn>
2026-01-30 18:06:45 +0800

    fix: 修复cwd

--- bin/start.sh
+++ bin/start.sh
@@ -3,5 +3,5 @@ set -e
 echo "Starting Frontend"
 nginx -c /Users/zf-server1/Desktop/MiniCi/apps/web/nginx.conf
 echo "starting Backend"
-pm2 start apps/server/dist/app.js --name "MiniCI"
+(cd apps/server && pm2 start dist/app.js --name "MiniCI")
 echo "Started successfully"
